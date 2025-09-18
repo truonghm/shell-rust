2a3c73564708adab2d837cdafefeb7a6fce89dd3
truonghm <truong173@outlook.com>
2025-09-19 00:03:33 +0700

    repl

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,14 @@
 use std::io::{self, Write};
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    while true {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    // Wait for user input
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-    println!("{}: command not found", input.trim())
+        // Wait for user input
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        println!("{}: command not found", input.trim())
+    }
+
 }
