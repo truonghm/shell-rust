088529a5712882dc48d43a7c9dc434e3516b1f7e
truonghm <truong173@outlook.com>
2025-09-19 00:37:00 +0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,19 @@ fn main() {
 
         // Wait for user input
         let mut input = String::new();
+
+        // receive new input after this line
         io::stdin().read_line(&mut input).unwrap();
         if input.trim() == "exit 0" {
             return;
+        } else if input.trim().starts_with("echo") {
+            if let Some(echo_text) = input.trim().strip_prefix("echo") {
+                println!("{}", echo_text.trim());
+            }
+        } else {
+            println!("{}: command not found", input.trim());
         }
-        println!("{}: command not found", input.trim());
+        
     }
 
 }
