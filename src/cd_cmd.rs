54ce025ff41c71d633bba132e99dc96d99783152
truonghm <truong173@outlook.com>
2025-09-20 22:28:51 +0700

    codecrafters submit [skip ci]

--- src/cd_cmd.rs
+++ src/cd_cmd.rs
@@ -2,7 +2,14 @@
 use std::env;
 
 pub fn change_directory(path: &str) {
-	match env::set_current_dir(path) {
+	let path_expanded: String;
+	if path == "~" || path.starts_with("~/") {
+		let home = env::var("HOME").unwrap_or("/".to_string());
+		path_expanded = path.replacen("~", &home, 1);
+	} else {
+		path_expanded = path.to_string()
+	}
+	match env::set_current_dir(path_expanded) {
 		Ok(()) => (),
 		Err(_) => println!("cd: {}: No such file or directory", path),
 	}
